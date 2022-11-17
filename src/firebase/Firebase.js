5c96e921f510e6f0c883524edadf73c37cf5d8d2
TuruuSE <gturmunkh30@gmail.com>
2022-11-17 10:42:45 +0800

    Last Push

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -5,23 +5,14 @@ import { getFirestore } from "@firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5FdzEinWwp4MP5ByfhHBCF03QWVR5f6g",
-  authDomain: "choijin-temple.firebaseapp.com",
-  projectId: "choijin-temple",
-  storageBucket: "choijin-temple.appspot.com",
-  messagingSenderId: "184673010246",
-  appId: "1:184673010246:web:b9b02c59bb1d20a3bec047",
-  measurementId: "G-VHYS3Y6015"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTD,
 };
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_APIKEY,
-//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-//   projectId: process.env.REACT_APP_PROJECTID,
-//   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-//   appId: process.env.REACT_APP_APPID,
-//   measurementId: process.env.REACT_APP_MEASUREMENTD,
-// };
 
 const app = initializeApp(firebaseConfig);
 export const authentication = getAuth(app);
